773e1349da37f090663b4e2e84915e04e5d063d7
Drian <nkoadrian@gmail.com>
2024-05-15 16:30:26 -0400

    code ready v.1

--- app_five.py
+++ app_five.py
@@ -3,23 +3,29 @@ import plotly.express as px
 import streamlit as st  
 
 car_data = pd.read_csv('vehicles_us.csv')
-hist_button = st.button('Construir histograma')
+
+st.header('AUTOS EN VENTA')
+
+hist_button = st.button('Histograma de cantidad de autos por su año')
 
 if hist_button:
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-    fig = px.histogram(car_data, x="odometer")
+    st.write('Autos en venta según año de fabricación')
+    fig = px.histogram(car_data, 
+                     x='model_year', 
+                    labels={"model_year": "Año del auto"}, 
+                    color_discrete_sequence=["green"])
     st.plotly_chart(fig, use_container_width=True)
 
 scatter_button = st.button('Construcción dispersión')
 
 if scatter_button:
-    st.write('Crea el gráfico de dispersión')
-    figu = px.scatter(car_data, x='model', y='model_year')
-    st.plotly_chart(figu, use_container_width=True)
+    st.write('Dispersión de costo según el año')
+    fig_two = px.scatter(car_data, 
+                        x='model_year', 
+                        y='price',
+                        color_discrete_sequence=["red"], 
+                        opacity=0.4,
+                        labels={"model_year": "Año del modelo", 'price':'Precio de Venta'})
+    st.plotly_chart(fig_two, use_container_width=True)
 
-price_button = st.button('Costo por km')
 
-if price_button:
-    st.write('Crea el gráfico de dispersión')
-    figu = px.scatter(car_data, x='model_year', y='price')
-    st.plotly_chart(figu, use_container_width=True)
